d12b6da6f0438fa23f4935e316bcccfa5ff57485
sushant <sushantp1927@gmail.com>
2025-11-13 13:50:32 +0530

    style change

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ children, ...props }) {
   return (
     <button
-      className=" py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
+      className=" px-2 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
       {...props}
     >
       {children}
